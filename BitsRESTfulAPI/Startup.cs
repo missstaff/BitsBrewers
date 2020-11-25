50a0eec265af129a09773694314046b8c3bf38da
Shawna Staff <staffs@my.lanecc.edu>
2020-11-24 22:57:16 -0800

    postman ajax calls went out were retrieved without changing port, also when sent post the message came back no content yet when I retrieved that item it had indeed been updated, all screenshots included

---
+++ BitsRESTfulAPI/Startup.cs
@@ -0,0 +1,68 @@
+using BitsBrewers.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BitsRESTfulAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // add cors policy - in a production app lock this down!
+            services.AddCors(options => {
+                options.AddDefaultPolicy(builder => {
+                    builder.AllowAnyOrigin()
+                    .WithMethods("POST", "PUT", "DELETE", "GET", "OPTIONS")
+                    .AllowAnyHeader();
+                });
+            });
+            // adding the dbContext to the service
+            services.AddDbContext<BitsContext>();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            // in a production app you would want to turn this back on!
+            // app.UseHttpsRedirection();
+
+            // enables the cors policy
+            app.UseCors();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
