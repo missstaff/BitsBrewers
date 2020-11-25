af09f089efee2e8f23d8b3cbcab17b1cc3211ab7
Shawna Staff <staffs@my.lanecc.edu>
2020-11-24 16:34:07 -0800

    First commit

---
+++ BitsBrewers/ConfigDB.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace BitsBrewers
+{
+    class ConfigDB
+    {
+        public static string GetMySqlConnectionString()
+        {
+            string folder = System.AppContext.BaseDirectory;
+            var builder = new ConfigurationBuilder()
+                    .SetBasePath(folder)
+                    .AddJsonFile("mySqlSettings.json", optional: true, reloadOnChange: true);
+
+            string connectionString = builder.Build().GetConnectionString("mySql");
+
+            return connectionString;
+        }
+    }
+}
