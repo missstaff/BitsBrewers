bcea931e7e8d9eed53e63807892a2f5f154f3ff8
Shawna Staff <staffs@my.lanecc.edu>
2020-11-24 17:00:42 -0800

    first test passes!; only had to start over twice lol

--- BitsBrewersUnitTests/AdjunctTests.cs
+++ BitsBrewersUnitTests/AdjunctTests.cs
@@ -1,18 +1,32 @@
+using BitsBrewers.Models;
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BitsBrewersUnitTests
 {
+    [TestFixture]
     public class Tests
     {
+        BitsContext dbContext;
+        Adjunct a;
+        List<Adjunct> adjuncts;
+
         [SetUp]
         public void Setup()
         {
+            dbContext = new BitsContext();
         }
 
         [Test]
-        public void Test1()
+        public void GetAllTest()
         {
-            Assert.Pass();
+            adjuncts = dbContext.Adjunct.OrderBy(a => a.AdjunctType).ToList();
+            Assert.AreEqual(130, adjuncts.Count);
+            Assert.AreEqual("'Antioxident'", adjuncts[4].UseFor);
+            //PrintAll(adjuncts);
         }
+
     }
 }
