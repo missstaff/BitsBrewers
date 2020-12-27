4f6d987b96e1be917fdc9ff07a8d6bac24a0c259
Shawna Staff <staffs@my.lanecc.edu>
2020-12-27 15:25:12 -0800

    scheduled list populates from local storage, find recipe searches and retrieves recipe by name. Now I wouldlike to add an on touch handler: when the calendar icon is touched I would like that recipe moved to the scheduled list and by default set brewing to N

--- Manifest Beer Co/src/index.js
+++ Manifest Beer Co/src/index.js
@@ -43,8 +43,11 @@ class Index {
             this.schedule = JSON.parse(localStorage["RECIPES"]);
             this.fillScheduledRecipesList = this.fillScheduledRecipesList.bind(this);
             this.fillScheduledRecipesList();
+            //this is for adding a recipe to schedule array
+           // document.getElementById('calendar').ontouchend = this.push.bind(this);
     }
 
+    //api call for find recipe(lower search bar)//
     onFormSubmit(event) {
         event.preventDefault();
         let rName = document.getElementById('search-add').value;
@@ -66,7 +69,8 @@ class Index {
     }
     //note to self use calendar icon in lower table to put in array and display at top of page~do not use brew schedule page yet!
 
-    //displays recipe(s) in the search for recipe table~these get added to the schedule table by pressing the calendar button//
+    //displays recipe(s) in the search for recipe table~these 
+    //get added to the schedule table by pressing the calendar button//
     renderRecipe(recipe, index) {
         let name = recipe.name;
         let style = recipe.style;
@@ -87,7 +91,7 @@ class Index {
                 <td>Y/N</td>
                 <td><a class="btn btn-default button1" href="">QOH</a></td>
                 <td><a class="btn btn-default button1" href="history.html">History</a></td>
-                <td><a class="btn btn-default button1" href="ingredients.html">Ingredients</a></td>
+                <td><a class="btn btn-default button1" id="calendar" href="ingredients.html">Ingredients</a></td>
                 <!--ideally this icon would display expected date of completion(red) or start(green)-->
                 <td><a class="btn btn-default button2 ml-3" href="">
                         <svg width="2.5em" height="2.5em" viewBox="0 0 16 16" class="bi bi-calendar" fill="currentColor"
@@ -106,13 +110,41 @@ class Index {
         document.getElementById('search-add').value = "";
     }
 
+    generateRecipeHtml(recipe, index) {
+        return `
+        <tbody id="schedule">
+        <tr>
+            <td>${recipe.name}</td>
+            <td>${recipe.style}</td>
+            <td>${recipe.version}</td>
+            <td>${recipe.ibu}</td>
+            <td>${recipe.abv}</td>
+            <td><a class="btn btn-default button1" href="history.html">History</a></td>
+            <td><a class="btn btn-default button1" href="ingredients.html">Ingredients</a></td>
+            <td><a class="btn btn-default button1" href="container.html">Container</a></td>
+            <td><a class="btn btn-default button1" href="qoh.html">QOH</a></td>
+            <td>Y/N</td>
+            <!--ideally this icon would display expected date of completion(red) or start(green)-->
+            <td><a class="btn btn-default button2 ml-3" href="">
+                    <svg width="2.5em" height="2.5em" viewBox="0 0 16 16" class="bi bi-calendar" fill="currentColor"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+                    </svg>
+                </a>
+            </td>
+        </tr>
+        </tbody>
+        `;
+    }
+
     fillScheduledRecipesList() {
         localStorage.setItem("RECIPES", JSON.stringify(this.schedule));
  
-        /*let recipeHtml = this.schedule.reduce(
+       let recipeHtml = this.schedule.reduce(
             (html, recipe, index) => html += this.generateRecipeHtml(recipe, index),
             '');
-        document.getElementById("brew").innerHTML = recipeHtml;*/
+        document.getElementById("schedule").innerHTML = recipeHtml;
     }
 
 }
